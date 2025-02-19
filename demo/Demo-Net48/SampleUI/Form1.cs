df7eaab0d106aa613eba13cec356e17e0f6e0fdf
Onkel Mato <thomas-ley@outlook.de>
2025-02-19 16:55:57 +0100

    BindableButton for .NET4.8 so it supports command binding

---
+++ demo/Demo-Net48/SampleUI/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SampleUI
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MagicCommand = new MessageBoxCommand();
+
+            bindableButton1.DataBindings.Add(
+                new Binding("CommandParameter", this, null, true));
+            bindableButton1.DataBindings.Add(
+                new Binding(nameof(bindableButton1.Command), this, nameof(MagicCommand), true));
+        }
+
+        public ICommand MagicCommand { get; set; }
+    }
+}
---
+++ demo/Demo-Net48/SampleUI/MessageBoxCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SampleUI
+{
+    public class MessageBoxCommand : ICommand
+    {
+
+        private bool _neverExecuted = true;
+        public bool CanExecute(object parameter)
+        {
+            return _neverExecuted;
+        }
+
+        public void Execute(object parameter)
+        {
+            _neverExecuted = false;
+            OnCanExecuteChanged();
+
+            MessageBox.Show(@"Hello World!");
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
