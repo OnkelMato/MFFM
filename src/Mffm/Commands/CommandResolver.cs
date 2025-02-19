1528a89aa1825ab36929e3a7ebeb1f8907a4eb59
Onkel Mato <46560885+OnkelMato@users.noreply.github.com>
2025-02-19 16:33:29 +0100

    command resolver added (#5)

---
+++ src/Mffm/Commands/CommandResolver.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+using Mffm.Contracts;
+
+namespace Mffm.Commands
+{
+    internal class CommandResolver(IServiceProvider serviceProvider) : ICommandResolver
+    {
+        private readonly IServiceProvider _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+
+        public ICommand ResolveCommand<TCommand>()
+            where TCommand : ICommand
+        {
+
+            var command = (ICommand)_serviceProvider.GetService(typeof(TCommand))!;
+            if (command == null)
+            {
+                throw new InvalidOperationException($"Command '{typeof(TCommand).Name}' not found.");
+            }
+
+            return command;
+        }
+    }
+}
