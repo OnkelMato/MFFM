f1f5992500af89f0e0d86ca95f7a58f1fc7c64eb
Thomas Ley <thomas-ley@outlook.de>
2025-03-12 19:21:09 +0100

    combobox and treeview added to bindings. bug fixed wit hreset

--- src/Mffm/Core/Bindings/BindingExtensions.cs
+++ src/Mffm/Core/Bindings/BindingExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Mffm.Core.Bindings;
+﻿using Mffm.Contracts;
+
+namespace Mffm.Core.Bindings;
 
 internal static class BindingExtensions
 {
@@ -11,5 +13,17 @@ internal static class BindingExtensions
     {
         return source.Cast<Binding>()
             .Any(dataBinding => dataBinding.PropertyName == propertyName);
+    }
+
+    internal static bool TryAddBinding(this ControlBindingsCollection source,
+        string controlProperty, IFormModel formModel, string formModelProperty,
+        bool allowFormatting = true, DataSourceUpdateMode updateMode = DataSourceUpdateMode.OnPropertyChanged)
+    {
+        // check if binding already exists. This is important when more than one IBinding is applied to the same controlProperty.
+        if (source.HasBindingFor(controlProperty)) return false;
+
+        source.Add(new Binding(controlProperty, formModel, formModelProperty, allowFormatting, updateMode));
+
+        return true;
     }
 }
